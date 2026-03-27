597ed4560796d7037eacfc44823cad44f36540c0
bestcast <support@bestcaststudios.com>
2026-03-27 17:07:19 +0530

    27 March 2026 TWO

--- lib/streamingpalyer/video_view_player.dart
+++ lib/streamingpalyer/video_view_player.dart
@@ -216,7 +216,7 @@ class _MovieVideoViewerState extends State<MovieVideoViewer> {
             movieId: widget.getMainMovieID,
             attemptId: _quizResponse?.attemptId ?? "",
             token: _token,
-            durationSeconds: _quizResponse!.questions[_currentQuizIndex].showQuestionTime,
+            durationSeconds: 10, // Hardecoded 10 sec question popup time
             onComplete: () {
               setState(() {
                 _isQuizActive = false;
